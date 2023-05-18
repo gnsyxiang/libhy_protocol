26e095268e9886054b3435f8e4aae5261ca30baf
zhenquan.qiu <gnsyxiang@163.com>
2023-05-18 10:30:31 +0800

    feat: add socket dir and files

---
+++ include/socket/socket_client.h
@@ -0,0 +1,32 @@
+/**
+ *
+ * Release under GPLv-3.0.
+ * 
+ * @file    socket_client.h
+ * @brief   
+ * @author  gnsyxiang <gnsyxiang@163.com>
+ * @date    18/05 2023 10:26
+ * @version v0.0.1
+ * 
+ * @since    note
+ * @note     note
+ * 
+ *     change log:
+ *     NO.     Author              Date            Modified
+ *     00      zhenquan.qiu        18/05 2023      create the file
+ * 
+ *     last modified: 18/05 2023 10:26
+ */
+#ifndef __LIBHY_PROTOCOL_INCLUDE_SOCKET_CLIENT_H_
+#define __LIBHY_PROTOCOL_INCLUDE_SOCKET_CLIENT_H_
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+#ifdef __cplusplus
+}
+#endif
+
+#endif
+
---
+++ src/socket/socket_client.c
@@ -0,0 +1,22 @@
+/**
+ * 
+ * Release under GPLv-3.0.
+ * 
+ * @file    socket_client.c
+ * @brief   
+ * @author  gnsyxiang <gnsyxiang@163.com>
+ * @date    18/05 2023 10:25
+ * @version v0.0.1
+ * 
+ * @since    note
+ * @note     note
+ * 
+ *     change log:
+ *     NO.     Author              Date            Modified
+ *     00      zhenquan.qiu        18/05 2023      create the file
+ * 
+ *     last modified: 18/05 2023 10:25
+ */
+#include <stdio.h>
+
+#include "socket_client.h"
